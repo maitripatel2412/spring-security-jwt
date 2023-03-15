30308a0a73834afe4897a9435c6aa47821d7c8da
maitripatel2412 <105631449+maitripatel2412@users.noreply.github.com>
2023-03-15 15:10:22 +0530

    Add files via upload
    commit 1

---
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/HelloResource.java
@@ -0,0 +1,14 @@
+package io.javabrains.springsecurityjwt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloResource {
+
+    @RequestMapping({"/hello"})
+    public String hello(){
+        return "hello world";
+    }
+
+}
